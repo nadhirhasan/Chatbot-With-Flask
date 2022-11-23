0a80da859f025743acd6d7ba9f7aaad88fda5457
nadhirhasan <86673541+nadhirhasan@users.noreply.github.com>
2022-11-23 19:58:47 +0530

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,54 +1,54 @@
 import random
-import json
+# import json
 
-import torch
+# import torch
 
-from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+# from model import NeuralNet
+# from nltk_utils import bag_of_words, tokenize
 import time
 # import pyautogui
 from threading import Timer
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
-    intents = json.load(json_data)
+# with open('intents.json', 'r') as json_data:
+#     intents = json.load(json_data)
 
-FILE = "data.pth"
-data = torch.load(FILE)
+# FILE = "data.pth"
+# data = torch.load(FILE)
 
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
+# input_size = data["input_size"]
+# hidden_size = data["hidden_size"]
+# output_size = data["output_size"]
+# all_words = data['all_words']
+# tags = data['tags']
+# model_state = data["model_state"]
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
+# model = NeuralNet(input_size, hidden_size, output_size).to(device)
+# model.load_state_dict(model_state)
+# model.eval()
 
 bot_name = "Sam"
 
 def get_response(msg):
-    sentence = tokenize(msg)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+    # sentence = tokenize(msg)
+    # X = bag_of_words(sentence, all_words)
+    # X = X.reshape(1, X.shape[0])
+    # X = torch.from_numpy(X).to(device)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+    # output = model(X)
+    # _, predicted = torch.max(output, dim=1)
 
-    tag = tags[predicted.item()]
+    # tag = tags[predicted.item()]
 
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                return random.choice(intent['responses'])
+    # probs = torch.softmax(output, dim=1)
+    # prob = probs[0][predicted.item()]
+    # if prob.item() > 0.75:
+    #     for intent in intents['intents']:
+    #         if tag == intent["tag"]:
+    #             return random.choice(intent['responses'])
     
-    return "I do not understand..."
+    return "Response without Pytorch"
 
 
 # def break_func():
